fca6a845b1654ca225314984804c332ce0952f97
Michael Probst <mpprobst@gmail.com>
2021-11-04 00:31:59 -0400

    Implement LSTM

--- source/agents/models/lstm.py
+++ source/agents/models/lstm.py
@@ -2,6 +2,7 @@
 lstm.py
 Author: Michael Probst
 Purpose: Implements a lstm rnn
+refernece: https://blog.floydhub.com/long-short-term-memory-from-zero-to-hero-with-pytorch/
 """
 
 import torch as T
@@ -9,14 +10,45 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LSTM(nn.Module):
-    def __init__(self, dim_input, dim_hidden):
+    def __init__(self, dim_input, dim_output, dim_hidden, n_layers, drop_prob=0.5):
         super(LSTM, self).__init__()
-        self.lstm = nn.LSTM(dim_input, dim_hidden)
-        self.linear = nn.Linear(dim_hidden, dim_input)  # input size = output size
+        self.dim_input = dim_input # num features
+        self.dim_hidden = dim_hidden
+        self.dim_output = dim_output
+        self.n_layers = n_layers
 
-    def forward(self, data):
-        data = T.tensor(data)
-        out, _ = self.lstm(data)
-        lin_out = self.linear(out)
-        scores = F.log_softmax(lin_out, dim=1)
-        return scores
+        # note: skipping the embedding layer because notes are already embedded
+        self.embedding = nn.Embedding(dim_input, dim_output)
+        self.lstm = nn.LSTM(dim_output, dim_hidden, n_layers, dropout=drop_prob, batch_first=True)
+        self.dropout = nn.Dropout(drop_prob)
+        self.fc = nn.Linear(dim_hidden, dim_output)
+        self.sigmoid = nn.Sigmoid()
+
+        if T.cuda.is_available():
+            self.device = T.device("cuda")
+        else:
+            self.device = T.device("cpu")
+
+    def forward(self, data, hidden):
+        batch_size = data.size(0)
+        data = T.tensor(data).to(self.device).long()
+
+        embeds = self.embedding(data)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+        #print(f'lstm out {lstm_out.size()}')
+        lstm_out = lstm_out.contiguous().view(-1, self.dim_hidden)
+
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        out = self.sigmoid(out)
+        #print(f'before view: {out.size()}')
+        out = out.view(batch_size, 128, -1)
+        #print(f'after view: {out.size()}')
+        out = out[:,-1]
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        hidden = (weight.new(self.n_layers, batch_size, self.dim_hidden).zero_().to(self.device),
+                  weight.new(self.n_layers, batch_size, self.dim_hidden).zero_().to(self.device))
+        return hidden
